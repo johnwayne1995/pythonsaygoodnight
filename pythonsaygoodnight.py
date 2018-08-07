b7227119fba785326afc76252968a51c700d62d3
张巍 <zhangwei@zhangweideMacBook-Pro.local>
2018-08-07 17:03:27 +0800

    Initial commit

---
+++ pythonsaygoodnight.py
@@ -0,0 +1,46 @@
+#encoding:utf-8
+#pip install wxpy
+#pip install wechat_sender
+#pip install requests
+from __future__ import unicode_literals
+from threading import Timer
+from wxpy import *
+import requests
+
+bot = Bot()
+
+# linux执行登陆请调用下面的这句
+#bot = Bot(console_qr=2,cache_path="botoo.pkl")
+
+def get_news():
+
+    """获取金山词霸每日一句，英文和翻译"""
+
+    url = "http://open.iciba.com/dsapi/"
+    r = requests.get(url)
+    content = r.json()['content']
+    note = r.json()['note']
+    return content, note
+
+def send_news():
+    try:
+        contents = get_news()
+
+        # 你朋友的微信名称，不是备注，也不是微信帐号。
+
+        my_friend = bot.friends().search(u'一轮玫瑰')[0]
+        my_friend.send(contents[0])
+        my_friend.send(contents[1])
+        my_friend.send(u"good night!")
+        # 每86400秒（1天），发送1次
+        t = Timer(86400, send_news)
+        t.start()
+    except:
+
+        # 你的微信名称，不是微信帐号。
+
+        my_friend = bot.friends().search('Victor')[0]
+        my_friend.send(u"今天消息发送失败了")
+
+if __name__ == "__main__":
+    send_news()
